6b32e470272b5268240607edd1ecf1a9c1e3d928
Anandiaz <anandiazagung@gmail.com>
2024-10-22 15:00:58 +0800

    Review 7

--- project_django/framework1/framework1/settings.py
+++ project_django/framework1/framework1/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "ais",
     "django_bootstrap5",
-    "django_seed"
+    "django_seed",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -83,8 +84,9 @@ DATABASES = {
         "USER": "root",
         "PASSWORD": "",
         "HOST": "127.0.0.1",
-        "PORT": "3306"
+        "PORT": "3306",
     }
+    
 }
 
 
